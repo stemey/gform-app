49fd8711e3bc13f4ba4fcda3397da59c1cce1133
Stefan Meyer <stefan.meyer@sinnerschrader.com>
2015-04-21 18:29:57 +0200

    display doc in iframe

--- src/cms/controller/DocumentationPreviewer.js
+++ src/cms/controller/DocumentationPreviewer.js
@@ -1,13 +1,22 @@
 define([
-	'dijit/layout/ContentPane',
-	"dojo/_base/declare"
-], function (ContentPane, declare) {
+	'dijit/layout/_ContentPaneResizeMixin',
+	'gform/layout/_InvisibleMixin',
+	'dijit/_TemplatedMixin',
+	'dijit/_WidgetsInTemplateMixin',
+	'dojo/dom-attr',
+	'dijit/_WidgetBase',
+	"dojo/_base/declare",
+	"dojo/text!./DocumentationPreviewer.html"
+], function (ContentPaneResizeMixin, InvisibleMixin, TemplatedMixin, WidgetsInTemplateMixin, domAttr, _WidgetBase, declare, template) {
 
 
-	return declare([ContentPane], {
+	return declare([_WidgetBase, TemplatedMixin, WidgetsInTemplateMixin, ContentPaneResizeMixin], {
+		templateString:template,
 		previewerId: "documentation",
+		iframe:null,
 		postCreate: function () {
-			this.set("href", "cms/documentation/index.html");
+			domAttr.set(this.iframe,"src","cms/documentation/index.html");
+			//this.iframe.set("href", "cms/documentation/index.html");
 		}
 	});
 });
